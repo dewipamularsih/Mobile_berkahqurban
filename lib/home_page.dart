269588593f4c7ff5623a2eca98f9e65d1f6193ee
dewipamularsih <dewipamular04@gmail.com>
2024-07-10 10:15:31 +0700

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomePage> {
+  WebViewController? _controller;
+
+  @override
+  void initState(){
+    super.initState();
+    _controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setBackgroundColor(const Color(0x00000000))
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) {},
+      onHttpError: (HttpResponseError error) {},
+      onWebResourceError: (WebResourceError error) {},
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url.startsWith('https://www.youtube.com/')) {
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+    ),
+  )
+   ..loadRequest(Uri.parse('https://kelompokrplxmobile1.000webhostapp.com'));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: _controller!,),
+    );
+  }
+}
